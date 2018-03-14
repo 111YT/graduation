7ca6170e8cce9eb1ad1874361d642e4866f05c0c
lyt <1628129717@qq.com>
2018-03-14 12:13:57 +0800

    index

--- graduation/src/components/common/ft.vue
+++ graduation/src/components/common/ft.vue
@@ -1,64 +1,69 @@
 <template>
+<div class="ft">
   <div class="index-ft">
     <div class="ft-box">
-    <div class="footnav">
-      <ul>
-        <li>首页</li>
-        <li>首页</li>
-        <li>首页</li>
-        <li>首页</li>
-        <li>首页</li>
-        <li>首页</li>
-        <li>首页</li>
-      </ul>
+        <div class="footnav">
+          <ul>
+            <li>首页</li>
+            <li>首页</li>
+            <li>首页</li>
+            <li>首页</li>
+            <li>首页</li>
+            <li>首页</li>
+            <li>首页</li>
+          </ul>
+        </div>
+        <div class="logos">
+          <ul>
+            <li>
+              <img src="../../assets/img/dlogo1.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo2.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo3.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo4.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo5.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo6.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo7.png" alt="">
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src="../../assets/img/dlogo8.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo9.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo10.png" alt="">
+            </li>
+            <li>
+              <img src="../../assets/img/dlogo11.png" alt="">
+            </li>
+          </ul>
+        </div>
+        <div class="choiceus">联系我们/CONTECT US
+        </div>
+        <div class="choice-title">
+          <span>公司地址：上海市XX区XX路XX街XX号</span><span>邮箱：您的邮箱@163.com</span>
+          <span>电话：021-XXXX-XXXX</span>
+          <p>热线：400- 000</p>
+        </div>
     </div>
-    <div class="logos">
-      <ul>
-        <li>
-          <img src="../../assets/img/dlogo1.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo2.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo3.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo4.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo5.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo6.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo7.png" alt="">
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <img src="../../assets/img/dlogo8.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo9.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo10.png" alt="">
-        </li>
-        <li>
-          <img src="../../assets/img/dlogo11.png" alt="">
-        </li>
-      </ul>
-    </div>
-    <div class="choiceus">联系我们/CONTECT US</div>
-    </div>
-    <div class="choice-title">
-<span>公司地址：上海市XX区XX路XX街XX号</span><span>邮箱：您的邮箱@163.com</span>
-<span>电话：021-XXXX-XXXX</span>
-<p>热线：400- 000</p>
-
-    </div>
+  </div>
+  <div class="ft-down">
+  Copyright©2017-现在 Hangzhou GreenCloud Tech Co., Ltd. All Rights Reserved.
+  </div>
   </div>
 </template>
 
@@ -67,70 +72,83 @@ export default {};
 </script>
 
 <style lang='less'>
-.index-ft {
-  width: 100%;
-  height: 400px;
-  background-color: red;
-  .ft-box {
-    padding-top: 1px;
-    margin: 0 auto;
-    width: 1200px;
-    height: auto;
-    .footnav {
-      margin-top: 50px;
-      ul {
-        margin: 0 15px;
-        border-bottom: 1px solid rgb(67, 67, 67);
-        border-top: 1px solid rgb(67, 67, 67);
-
-        li {
-          display: inline-block;
-          line-height: 4;
-          width: 100px;
-          height: 50px;
+.ft{
+  .index-ft {
+    width: 100%;
+    height: 440px;
+    // background-image: url('../../assets/img/ft.jpg');
+        background-color: #f5f5f5;
+    // background-color: red;
+    .ft-box {
+      padding-top: 1px;
+      margin: 0 auto;
+      width: 1200px;
+      height: auto;
+      .footnav {
+        margin-top: 50px;
+        ul {
           margin: 0 15px;
-          text-align: center;
+          border-bottom: 1px solid rgb(67, 67, 67);
+          border-top: 1px solid rgb(67, 67, 67);
+
+          li {
+            display: inline-block;
+            line-height: 4;
+            width: 100px;
+            height: 50px;
+            margin: 0 15px;
+            text-align: center;
+          }
         }
       }
-    }
-    .logos {
-      ul {
-        display: inline-block;
-        margin: 0 auto;
-        padding: 15px 0;
-        border-bottom: 2px solid #ddd;
-        height: 76px;
-        width: auto;
-        li {
-          margin-left: 35px;
+      .logos {
+        ul {
           display: inline-block;
-          img {
-            -moz-filter: grayscale(100%);
-            -ms-filter: grayscale(100%);
-            -o-filter: grayscale(100%);
-            filter: grayscale(100%);
-            filter: gray;
-            &:hover{
-             -moz-filter: grayscale(0%);
-            -ms-filter: grayscale(0%);
-            -o-filter: grayscale(0%);
-            filter: grayscale(0%);
-            filter: gray;
+          margin: 0 auto;
+          padding: 15px 0;
+          // border-bottom: 2px solid #ddd;
+          height: 76px;
+          width: auto;
+          li {
+            margin-left: 35px;
+            display: inline-block;
+            img {
+              -moz-filter: grayscale(100%);
+              -ms-filter: grayscale(100%);
+              -o-filter: grayscale(100%);
+              filter: grayscale(100%);
+              filter: gray;
+              &:hover{
+               -moz-filter: grayscale(0%);
+              -ms-filter: grayscale(0%);
+              -o-filter: grayscale(0%);
+              filter: grayscale(0%);
+              filter: gray;
+              }
             }
           }
         }
       }
-    }
-    .choiceus{
-      font-size: 18px;
-      margin-top: 20px;
-    }
-    .choice-title{
-      margin-top: 20px;
-      span{
-        margin-left: 20px;
+      .choiceus{
+        font-size: 18px;
+        margin-top: 20px;
+      }
+      .choice-title{
+        // margin-top: 20px;
+        margin: 15px 0 15px;
+        span{
+          margin-left: 20px;
+        }
       }
     }
+  }
+  .ft-down{
+    width: 100%;
+    height: 60px;
+    background-color: #000;
+    text-align: center;
+    line-height: 64px;
+    color: #fff;
   }
 }
 </style>
--- graduation/src/components/page/index.vue
+++ graduation/src/components/page/index.vue
@@ -2,132 +2,129 @@
 <div class="box">
      <banner></banner>
       <div class="server-box">
-         <div class="server-title">服务项目/SERVICE ITEMS</div>
-        <div class="server-text">上千套房间任君挑选，给你奇妙之旅自助退订，科技服务，舒适有度，当之无愧，让酒店回归居家本质</div>
-            <ul>
-          <li>
-            <div class="font-bx">
-            <i class="iconfont icon-nav-self-service"></i>
+         <div class="server-title wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">服务项目/SERVICE ITEMS</div>
+        <div class="server-text wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">上千套房间任君挑选，给你奇妙之旅自助退订，科技服务，舒适有度，当之无愧，让酒店回归居家本质</div>
+            <ul class=" wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">
+          <li :style="image1">
+            <div class="mask"></div>
+            <div class="list-box">
+                <div class="font-bx" >
+                <i class="iconfont icon-nav-self-service"></i>
+                </div>
+                <div class="list-title">住宿</div>
+                <div class="list-text">Get accommodation</div>
             </div>
-            <div class="list-title">住宿</div>
-            <div class="list-text">Get accommodation</div>
           </li>
-          <li>
-            <div class="font-bx">
-            <i class="iconfont icon-Hotel"></i>
+          <li :style="image2">
+            <div class="mask"></div>
+            <div class="list-box">
+              <div class="font-bx">
+              <i class="iconfont icon-Hotel"></i>
+              </div>
+              <div class="list-title">服务式套房</div>
+              <div class="list-text">SERVICE SUITE</div>
             </div>
-            <div class="list-title">服务式套房</div>
-            <div class="list-text">SERVICE SUITE</div>
           </li>
-          <li>
+          <li :style="image3">
+            <div class="mask"></div>
+            <div class="list-box">
             <div class="font-bx">
             <i class="iconfont icon-nav-self-service"></i>
             </div>
             <div class="list-title">贵宾阁</div>
             <div class="list-text">VIP &nbsp; CLUB</div>
+            </div>
           </li>
-          <li>
+          <li :style="image4">
+            <div class="mask"></div>
+            <div class="list-box">
             <div class="font-bx">
             <i class="iconfont icon-huiyikaihuitaolun"></i>
             </div>
             <div class="list-title">会议</div>
             <div class="list-text">MEETING</div>
+            </div>
           </li>
-          <li>
+          <li :style="image5">
+            <div class="mask"></div>
+            <div class="list-box">
             <div class="font-bx">
             <i class="iconfont icon-canting"></i>
             </div>
             <div class="list-title">餐厅及酒吧</div>
             <div class="list-text">RESTAURANT &nbsp; BAR</div>
+            </div>
           </li>
-          <li>
+          <li :style="image6">
+            <div class="mask"></div>
+            <div class="list-box">
             <div class="font-bx">
             <i class="iconfont icon-guojijiudianchanpin"></i>
             </div>
             <div class="list-title">设施</div>
             <div class="list-text">FACILITIES</div>
+            </div>
           </li>
         </ul>
       </div>
       <div class="server-box">
-        <div class="server-title">服务项目/SERVICE ITEMS</div>
-        <div class="server-text">上千套房间任君挑选，给你奇妙之旅自助退订，科技服务，舒适有度，当之无愧，让酒店回归居家本质</div>
+        <div class="server-title wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">服务项目/SERVICE ITEMS</div>
+        <div class="server-text wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">上千套房间任君挑选，给你奇妙之旅自助退订，科技服务，舒适有度，当之无愧，让酒店回归居家本质</div>
         <div class="roomul">
           <ul>
-            <li>
+            <li class="wow slideInUp" data-wow-duration="1s" data-wow-delay="0.5s">
                <div class="room-img">
-                 <img src="" alt="">
+                 <img src="../../assets/img/room1.jpg" alt="">
                </div>
                <div class="room-title">豪华海景套房</div>
                <span>立即预订</span>
             </li>
-            <li>
+            <li class="wow slideInUp" data-wow-duration="1s" data-wow-delay="0.7s">
                <div class="room-img">
-                 <img src="" alt="">
+                 <img src="../../assets/img/room2.jpg" alt="">
                </div>
-               <div class="room-title">豪华海景套房</div>
+               <div class="room-title">商务经济房</div>
                <span>立即预订</span>
             </li>
-            <li>
+            <li class="wow slideInUp" data-wow-duration="1s" data-wow-delay="0.9s">
                <div class="room-img">
-                 <img src="" alt="">
+                 <img src="../../assets/img/room3.jpg" alt="">
                </div>
-               <div class="room-title">豪华海景套房</div>
+               <div class="room-title">精致商务房</div>
                <span>立即预订</span>
             </li>
-            <li>
+            <li class="wow slideInUp" data-wow-duration="1s" data-wow-delay="0.5s">
                <div class="room-img">
-                 <img src="" alt="">
+                 <img  src="../../assets/img/room4.jpg" alt="">
                </div>
-               <div class="room-title">豪华海景套房</div>
+               <div class="room-title">豪华大床房房</div>
                <span>立即预订</span>
             </li>
-            <li>
+            <li class="wow slideInUp" data-wow-duration="1s" data-wow-delay="0.8s">
                <div class="room-img">
-                 <img src="" alt="">
+                 <img src="../../assets/img/room5.jpg" alt="">
                </div>
-               <div class="room-title">豪华海景套房</div>
+               <div class="room-title">行政套房</div>
                <span>立即预订</span>
             </li>
-            <li>
+            <li class="wow slideInUp" data-wow-duration="1s" data-wow-delay="1.1s">
                <div class="room-img">
-                 <img src="" alt="">
+                 <img src="../../assets/img/room6.jpg" alt="">
                </div>
-               <div class="room-title">豪华海景套房</div>
-               <span>立即预订</span>
-            </li>
-            <li>
-               <div class="room-img">
-                 <img src="" alt="">
-               </div>
-               <div class="room-title">豪华海景套房</div>
-               <span>立即预订</span>
-            </li>
-            <li>
-               <div class="room-img">
-                 <img src="" alt="">
-               </div>
-               <div class="room-title">豪华海景套房</div>
-               <span>立即预订</span>
-            </li>
-            <li>
-               <div class="room-img">
-                 <img src="" alt="">
-               </div>
-               <div class="room-title">豪华海景套房</div>
+               <div class="room-title">豪华商务套房</div>
                <span>立即预订</span>
             </li>
           </ul>
         </div>
       </div>
       <div class="server-box">
-        <div class="server-title">新闻/NEWS</div>
-        <div class="server-text">NEWS</div>
+        <div class="server-title wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">新闻/NEWS</div>
+        <div class="server-text wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">NEWS</div>
         <div class="news-box clearfix">
-          <div class="newsL">
-            <img src="" alt="">
+          <div class="newsL wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+            <img src="../../assets/img/newsL.jpg" alt="">
           </div>
-          <div class="newsR">
+          <div class="newsR wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
             <ul>
               <li></li>
               <li></li>
@@ -138,6 +135,22 @@
         </div>
       </div>
       <ft></ft>
+      <div class="rightNav">
+        	<b class="close">×</b>
+					<div class="searchR">
+						<i class="iconfont icon-sousuo"></i>
+						<input type="text" name="" >
+					</div>
+					<div class="searchR-list">
+              <ul>
+                  <li><a href="/">MEETINGS &amp; EVENTS</a></li>
+                  <li><a href="/">OFFERS</a></li>
+                  <li><a href="/">DINING &amp; ENTERTAINMENT</a></li>
+                  <li><a href="/">EXPLORE US</a></li>
+              </ul>
+					</div>
+			</div>
+
 </div>
 </template>
 
@@ -146,7 +159,26 @@ import banner from "../common/banner";
 import ft from "../common/ft";
 export default {
   data() {
-    return {};
+    return {
+      image1: {
+        background: "url(" + require("../../assets/img/server1.jpg") + ")"
+      },
+      image2: {
+        background: "url(" + require("../../assets/img/server2.jpg") + ")"
+      },
+      image3: {
+        background: "url(" + require("../../assets/img/server3.jpg") + ")"
+      },
+      image4: {
+        background: "url(" + require("../../assets/img/server4.jpg") + ")"
+      },
+      image5: {
+        background: "url(" + require("../../assets/img/server5.jpg") + ")"
+      },
+      image6: {
+        background: "url(" + require("../../assets/img/server6.jpg") + ")"
+      }
+    };
   },
   components: {
     banner,
@@ -172,31 +204,52 @@ export default {
     ul {
       width: 100%;
       height: 440px;
-      margin-top: 25px;
+      margin-top: 40px;
       li {
+        position: relative;
         display: inline-block;
         margin: 0 10px;
         width: 185px;
         height: 440px;
-        // background-color: red;
         padding-top: 1px;
-        .font-bx {
-          margin-top: 120px;
-          i {
-            font-size: 56px;
+        &:hover {
+          .mask {
+            background-color: rgba(0, 0, 0, 0);
           }
         }
-        .list-title {
-          margin-top: 44px;
-          font-size: 18px;
-          font-weight: bold;
+        .mask {
+          position: absolute;
+          top: 0;
+          width: 185px;
+          height: 441px;
+          background: rgba(0, 0, 0, 0.4);
         }
-        .list-text {
-          margin-top: 5px;
+        .list-box {
+          width: 185px;
+          height: 440px;
+          position: absolute;
+          .font-bx {
+            margin-top: 120px;
+            i {
+              font-size: 56px;
+              color: #feda00;
+            }
+          }
+          .list-title {
+            margin-top: 44px;
+            font-size: 18px;
+            font-weight: bold;
+            color: #fff;
+          }
+          .list-text {
+            color: #fff;
+            margin-top: 5px;
+          }
         }
       }
     }
     .news-box {
+      margin-top: 40px;
       .newsL {
         float: left;
         width: 450px;
@@ -206,7 +259,9 @@ export default {
         float: right;
         ul {
           width: 700px;
+          margin-top: 0px;
           li {
+            margin-bottom: 0px;
             width: 700px;
             height: 100px;
             margin-left: 0px;
@@ -220,7 +275,7 @@ export default {
       ul {
         width: 100%;
         height: auto;
-        margin-top: 25px;
+        margin-top: 40px;
 
         li {
           overflow: hidden;
@@ -235,6 +290,7 @@ export default {
           }
           .room-title {
             margin-top: 30px;
+            margin-left: 15px;
             float: left;
             font-size: 14px;
             color: #666666;
@@ -252,6 +308,81 @@ export default {
               background-color: rgba(182, 51, 39, 1);
               color: #fff;
             }
+          }
+        }
+      }
+    }
+  }
+  .rightNav {
+    width: 356px;
+    border-left: 1px solid #716e6a;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    // right: -725px;
+    right: 0;
+    z-index: 1001;
+    // transition: right .4s;
+    // transition-timing-function: ease-in-out;
+    background-color: rgba(0, 0, 0, 0.9);
+    .close{
+          position: absolute;
+    right: 35px;
+    top: 40px;
+    color: #fff;
+    font-size: 30px;
+    font-weight: lighter;
+    cursor: pointer;
+    }
+    i {
+      position: absolute;
+      /* right: 35px; */
+      /* top: 5px; */
+      color: #353030;
+      font-size: 30px;
+      font-weight: lighter;
+      cursor: pointer;
+      left: 5px;
+      bottom: -6px;
+    }
+    .searchR {
+      width: 280px;
+      height: 35px;
+      background-color: #fff;
+      margin-top: 147px;
+      margin-left: 40px;
+      position: relative;
+      input {
+        width: 218px;
+        padding: 5px 10px 5px 35px;
+        line-height: 25px;
+        font-size: 14px;
+        color: #333;
+        border: 0;
+        vertical-align: middle;
+        outline: 0;
+      }
+    }
+    .searchR-list {
+      ul {
+        padding-left: 40px;
+        margin: 20px 0;
+        li {
+          width: 280px;
+          height: 60px;
+          line-height: 61px;
+          color: #fff;
+          font-size: 18px;
+          list-style: none;
+          margin-left: 0;
+          color: #fff;
+          text-align: left;
+          border-top: 1px solid #555352;
+            a{
+              color: #fff;
+            }
+          &::first-child {
+            border-top: 0;
           }
         }
       }
